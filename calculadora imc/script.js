adc2c12799cb5f3def45325d4bc05d7f87a06104
Felipe Alves de Jesus <felipoide.jesus@gmail.com>
2025-01-21 19:31:08 -0300

    Primeiro commit

---
+++ calculadora imc/script.js
@@ -0,0 +1,64 @@
+function calcular() {
+    var res = document.getElementById('formulario')
+    var nome = document.getElementById('nome').value;
+    var alt = document.getElementById('altura').value;
+    var x = Number(alt);
+    var peso = document.getElementById('peso').value ;
+    var y = Number(peso);
+    
+
+    var resultado = y/(x*x) //fazendo o calculo
+    var resultadoFormatado = resultado.toFixed(2)
+
+    if(alt >100){
+        alert("Por favor verifique se a sua altura está em metros.")
+        return
+    }
+
+    if(resultado < 18.5){       //definindo as condiçoes
+        res.textContent = nome + ', o resultado do seu imc indica que você está abaixo do peso: ' + resultadoFormatado
+
+    }
+
+    else if(resultadoFormatado >= 18.5 && resultadoFormatado <25){
+        res.textContent = nome + ', o resultado do seu imc indica que você está saúdavel: ' + resultadoFormatado
+    }
+    else if(resultadoFormatado > 25 && resultadoFormatado < 30){
+        res.textContent = nome + ', o resultado do seu imc indica que você está sobrepeso: ' + resultadoFormatado
+    }
+    else if(resultadoFormatado > 30){
+        res.textContent = nome + ', o resultado do seu imc indica que você está obeso: ' + resultadoFormatado
+    }
+
+    else{
+        alert("Por favor inserir os dados requisitados.")
+        return;
+        
+    }
+    
+    var botao_voltar = document.createElement("button") //criando um botao 
+  
+    botao_voltar.innerHTML = "voltar"
+    botao_voltar.style.marginTop = "20px";
+    botao_voltar.style.padding = "10px 20px";
+    botao_voltar.style.fontSize = "16px";
+    botao_voltar.style.cursor = "pointer" 
+    botao_voltar.style.backgroundColor = "#4CAF50"
+    botao_voltar.style.borderRadius = "5px"
+    botao_voltar.style.border = "0.5px solid black"
+    
+    botao_voltar.addEventListener("click", function(){ //função para voltar e calcular dnv
+        location.reload();
+    });
+
+    res.appendChild(botao_voltar);
+
+    res.style.display = "flex"; 
+    res.style.justifyContent = "center"; 
+    res.style.alignItems = "center"; 
+    res.style.textAlign = "center"; 
+    res.style.fontFamily = "Arial, Helvetica, sans-serif"
+
+
+    
+}
